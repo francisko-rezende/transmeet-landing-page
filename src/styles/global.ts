aab30ed79e59d0def89dc2f65145a3a05c5b80fb
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-16 17:27:50 -0300

    feat: setup webfonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,53 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+
+
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/inter-v8-latin-regular.woff2') format('woff2')
+}
+
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 700;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/inter-v8-latin-700.woff2') format('woff2')
+}
+
+@font-face {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 800;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/inter-v8-latin-800.woff2') format('woff2')
+}
+
+/* manrope-700 - latin */
+@font-face {
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 700;
+  src: local(''),
+       url('/fonts/manrope-v11-latin-700.woff2') format('woff2')
+}
+
+
+
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -mox-osx-font-smoothing: grayscale;
   }
 
   html {
@@ -16,7 +59,7 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 `
 
