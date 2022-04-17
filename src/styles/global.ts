d2f5c36cd286578f404ea2f76ec0c44306935d5d
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-17 10:43:46 -0300

    feat: hide body overflow in global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -57,7 +57,8 @@ const GlobalStyles = createGlobalStyle`
   } */
 
   body {
-    font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    overflow-x: hidden
   }
 
   ${({ theme }) => css`
