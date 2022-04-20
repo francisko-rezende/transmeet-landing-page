8fb7ac30faf95265e4cc239899c2ee8c8c5e2d7e
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-20 11:14:49 -0300

    chore: update page meta info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,14 +8,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Transmeet</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          name="Transmeet"
+          content="Através da nossa plataforma é possível encontrar empresas que estão dispostas a patrocinar os estudos da população trans, travesti e não binária em diversas áreas do conhecimento."
         />
       </Head>
       <GlobalStyles />
