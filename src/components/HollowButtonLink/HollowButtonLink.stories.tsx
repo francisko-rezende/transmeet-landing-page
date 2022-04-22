33d127364e2a6cb2af48e37b41987c123e10989e
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-21 21:58:33 -0300

    fix: change hollowButtonStories so build hopefully works

--- src/components/HollowButtonLink/HollowButtonLink.stories.tsx
+++ src/components/HollowButtonLink/HollowButtonLink.stories.tsx
@@ -9,10 +9,12 @@ export default {
   component: HollowButtonLink
 } as ComponentMeta<typeof HollowButtonLink>
 
-//const Template: ComponentStory<typeof HollowButtonLink> = (args) => <HollowButtonLink {...args} />
-
-const Template: ComponentStory<typeof HollowButtonLink> = () => (
-  <HollowButtonLink />
+const Template: ComponentStory<typeof HollowButtonLink> = (args) => (
+  <HollowButtonLink {...args} />
 )
+
+// const Template: ComponentStory<typeof HollowButtonLink> = () => (
+//   <HollowButtonLink />
+// )
 
 export const Default = Template.bind({})
