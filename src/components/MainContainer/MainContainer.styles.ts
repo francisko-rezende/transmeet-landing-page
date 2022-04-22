30470c087c33f34a8697a09d88d52ce1e1514ad3
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-22 19:48:35 -0300

    chore: clean up MainContainer files

--- src/components/MainContainer/MainContainer.styles.ts
+++ src/components/MainContainer/MainContainer.styles.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  /* max-width: ${({ theme }) => theme.grid.container}; */
   max-width: calc(1170px + 44px);
   padding: 0 22px;
   margin: 0 auto;
